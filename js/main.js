5e9231fbded5df05648630a11102d6e569c7bd29
Runatorium <stefano.alesci1@gmail.com>
2023-03-03 16:57:21 +0100

    esercizio base completato

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 const gridDom = document.getElementById('grid');
+const btnDom = document.getElementById('action-button')
+let play = 0;
 
-for(i=0; i < 100; i++){
-    const quadratocorrente = creanuovoquadrato();
 
-    quadratocorrente.addEventListener('click', function() {
-        this.classList.toggle('clicked');
-    });
+btnDom.addEventListener('click', function(){
+    for(i=0; i < 100; i++){
+        const quadratocorrente = creanuovoquadrato();
+        
+            quadratocorrente.addEventListener('click', function() {
+            this.classList.toggle('clicked');
+            
+        });
+        
+        gridDom.append(quadratocorrente);
+        quadratocorrente.append(i);
+     }
+
+});
     
 
-    gridDom.append(quadratocorrente);
-    quadratocorrente.append(i);
-}
-
+    
+       
+    
+       
+     
+    
 
 function creanuovoquadrato(){
     const currentElement = document.createElement('div');
